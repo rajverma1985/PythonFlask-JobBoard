936a28bf7fe642b64d084b0e55b8dce9db3a264b
Raj Verma <rajverma@Rajs-MacbookPro.local>
2020-03-15 22:17:47 +0530

    added connection and teardown info

--- jobs/app.py
+++ jobs/app.py
@@ -1,8 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, g
+import sqlite3
 #create an app instance using Flask constructor
-
+PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
+
+# connection to the databse
+def open_connection():
+    connection=getattr(g, '_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3(PATH)
+    connection.row_factory=sqlite3.Row
+    return connection
+
+#function to run the query
+
+def execute_sql(sql, values=(), commit=Fasle, single=Fasle):
+    connection=open_connection()
+    cursor=connection.execute(sql, values)
+    if commit==True:
+        results=connection.commit()
+    else:
+        results= cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection=getattr(g, '_connection', None)
+    if connection!=None:
+        connection.close()
+
+
+
 
 
 #basic route
